51e581f62625ed368d6ce64e03e0d800ad77172f
Carlos <carlos.r.goularte@hotmail.com>
2022-07-10 16:04:42 -0300

    Adicionado metodo insert

--- listaDuplamenteEncadeada/doublyLinkedList.py
+++ listaDuplamenteEncadeada/doublyLinkedList.py
@@ -12,13 +12,15 @@ class DoublyLinkedList:
         else:
             return False
 
+    def insertEmptyList(self,element):
+        node = Node(element)
+        self.head = self.tail = node
+        self.numElements += 1
+
     def append(self,element):
         if element:
             if self.isEmpty():
-                node = Node(element)
-                self.head = self.tail = node
-                self.numElements += 1
-                
+                self.insertEmptyList(element)
             else:
                 node = Node(element)
                 self.tail.next = node
@@ -29,11 +31,43 @@ class DoublyLinkedList:
             raise IndexError ("\nElement is null!\n")
 
     def insertHead(self,element):
-        pass
+        if element:
+            if self.isEmpty():
+                self.insertEmptyList(element)
+            else:
+                node = Node(element)
+                node.next = self.head
+                self.head.previous = node
+                self.head = node
+                self.numElements += 1
+        else:
+            raise IndexError ("\nElement is null!\n")
 
     def insert(self,element,position):
-        pass
+        if element:
+            if self.isEmpty():
+                self.insertEmptyList(element)
+            else:
+                if position == self.numElements:
+                    self.append(element)
+                elif position == 1:
+                    self.insertHead(element)
+                elif position == 0:
+                    print("Position does not exist!")
+                else:
+                    node = Node(element)
+                    nodeAux = self.head
+                    for index in range(position-2):
+                        nodeAux = nodeAux.next
+                    
+                    node.previous = nodeAux
+                    node.next = nodeAux.next
 
+                    nodeAux.next.previous = node
+                    nodeAux.next = node
+        else:
+            raise IndexError ("\nElement is null!\n")
+                
     def removeTail(self,element):
         pass
 
@@ -62,20 +96,37 @@ class DoublyLinkedList:
                 print(f"Element is: {node.data}")
                 node = node.previous
 
-            
+    def printHead(self):
+        print(f"Head is: {self.head.data}")
+
+    def printTail(self):
+        print(f"Tail is: {self.tail.data}")
+
     def printNumberElementsList(self):
         print("\nThe number of elements is: " + str(self.numElements))
 
 dList = DoublyLinkedList()
 
-# Teste Append
-#dList.append(10)
-#dList.append(15)
-#dList.append(20)
+# Test Insert Head
+#dList.insertHead(10)
+#dList.insertHead(20)
+#dList.insertHead(50)
 
-# Teste Print List
-#dList.printList(dList.head)
+# Test Append
+dList.append(10)
+dList.append(15)
+dList.append(20)
+dList.append(30)
+
+dList.insert(100,0)
+
+# Test Print List
+dList.printList(dList.head)
 #dList.printListInverse(dList.tail)
 
 
-#dList.printNumberElementsList()
+
+dList.printHead()
+dList.printTail()
+
+dList.printNumberElementsList()
