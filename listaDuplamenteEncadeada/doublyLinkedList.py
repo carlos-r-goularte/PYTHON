40086d5a760adfc736a314de0b3f5f693685768d
Carlos <carlos.r.goularte@hotmail.com>
2022-07-10 14:56:09 -0300

    Lista Duplamente Encadeada

---
+++ listaDuplamenteEncadeada/doublyLinkedList.py
@@ -0,0 +1,77 @@
+from node import Node
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = self.tail = None
+        self.numElements = 0
+
+    def isEmpty(self):
+        if self.head == self.tail == None:
+            return True
+        else:
+            return False
+
+    def append(self,element):
+        if element:
+            if self.isEmpty():
+                node = Node(element)
+                self.head = self.tail = node
+                self.numElements += 1
+                
+            else:
+                node = Node(element)
+                self.tail.next = node
+                node.previous = self.tail
+                self.tail = node
+                self.numElements += 1
+        else:
+            raise IndexError ("\nElemento nulo ou vazio\n")
+
+    def insertHead(self,element):
+        pass
+
+    def insert(self,element,position):
+        pass
+
+    def removeTail(self,element):
+        pass
+
+    def removeHead(self,element):
+        pass
+
+    def remove(self,element,position):
+        pass
+
+    def search(self,element):
+        pass
+    
+    def printList(self,node):
+        if self.isEmpty():
+            print("\nLISTA VAZIA\n")
+        else:
+            while (node is not None):
+                print(f"Element is: {node.data}")
+                node = node.next
+    
+    def printListInverse(self,node):
+        if self.isEmpty():
+            print("\nLISTA VAZIA\n")
+        else:
+            while (node is not None):
+                print(f"Element is: {node.data}")
+                node = node.previous
+
+            
+    def printNumberElementsList(self):
+        print("\nThe number of elements is: " + str(self.numElements))
+
+lista = DoublyLinkedList()
+
+lista.append(10)
+lista.append(15)
+lista.append(20)
+
+lista.printList(lista.head)
+
+lista.printNumberElementsList()
