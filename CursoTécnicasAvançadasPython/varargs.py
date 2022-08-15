4e22c2d62ca5821aca0c1044d18e8a2481e9e187
Carlos Rodrigo Goularte <84022202+carlos-r-goularte@users.noreply.github.com>
2022-08-15 17:35:41 -0300

    Add files via upload

--- CursoTécnicasAvançadasPython/varargs.py
+++ CursoTécnicasAvançadasPython/varargs.py
@@ -5,8 +5,11 @@ def main():
     print(adicao(5,10,20))
     print(adicao(1,2,2,2,4))
 
+    #passagem de lista como parametro
     valores = [5,10,15,20]
     print(adicao(*valores))
+
+
 if __name__ == "__main__":
     main()
 
